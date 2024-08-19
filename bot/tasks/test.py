b2ad3c90d0709728f91453927374a6b2fac7e0a5
canonnizq <canonnizq@gmail.com>
2024-08-19 14:14:09 +0800

    add some logoc

--- bot/tasks/test.py
+++ bot/tasks/test.py
@@ -5,4 +5,5 @@ page = pwb.Page(site, 'User:CanonNiBot/sandbox')
 
 new_text = page.text + '\n\n The quick brown fox jumps over the lazy dog'
 
-page.put(new_text, summary='test edit by bot')
+for i in range(10):
+    page.put(new_text, summary=f'test edit by bot, iteration {i}')
