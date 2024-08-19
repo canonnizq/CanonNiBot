3e878da0921b6c26474f9d176369d1bcb5224197
CanonNi <canonnizq@gmail.com>
2024-08-19 10:13:17 +0800

    Delete pywikibot directory

--- pywikibot/user-config.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-family = 'wikipedia'
-mylang = 'en'
-usernames['wikipedia']['en'] = 'CanonNiBot'
-
-authenticate['en.wikipedia.org'] = ('618f803a7fa25d377850a19a05820ef9','94f37066fc697b044263c2fa71171b4519a7fb05', '93ae2b6674117880a0c88124953c6301', '8c0579225685183133f016b63ac6f9c4ae8f5bd7')
